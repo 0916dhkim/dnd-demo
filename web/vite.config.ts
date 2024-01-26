7de5b7189cf3f886dc08ac9d4d6ec8f367d5c34e
Donghyeon Kim <0916dhkim@gmail.com>
2024-01-26 18:58:56 -0500

    Allow accessing dev server from outside

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0", // Allow external access
     proxy: {
       "/api": {
         target: "http://localhost:3000",
